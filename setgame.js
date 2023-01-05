61016c6f36b6ce597daa9dddeb9b9078ae63f343
liasly <mutjinsj2580@gmail.com>
2023-01-06 06:17:51 +0900

    Update setgame.js

--- setgame.js
+++ setgame.js
@@ -215,7 +215,11 @@ function setData(){
     left_num.innerHTML = Abnormality[leftab[0]].number;
 }
 
-
+function setCookie(name, value, exp) {
+    var date = new Date();
+    date.setTime(date.getTime() + exp*24*60*60*1000);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+}
 
 
 
